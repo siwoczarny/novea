f33e46a3762fe8fdc657a5d8420b17ff8c415f14
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-06-18 15:56:34 +0200

    Updates

--- precache-manifest.6c30c7826fa57e99cda7a4cb1ee27c90.js
+++ precache-manifest.6c30c7826fa57e99cda7a4cb1ee27c90.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "124302eb2ce8b6785e02e115843c2020",
+    "revision": "2fbf8cc8d75516e51cfe5451f9d45704",
     "url": "/novea/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/novea/static/js/2.db54d6bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0a66d4062c20cc17b2cc",
-    "url": "/novea/static/js/main.9f5c2a46.chunk.js"
+    "revision": "9609efa7a315ffb60b01",
+    "url": "/novea/static/js/main.f58ab3fb.chunk.js"
   },
   {
     "revision": "e58587895b70632461ad",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/novea/precache-manifest.3cabcf6417b007487a540f4ae9caefec.js"
+  "/novea/precache-manifest.6c30c7826fa57e99cda7a4cb1ee27c90.js"
 );
 
 self.addEventListener('message', (event) => {

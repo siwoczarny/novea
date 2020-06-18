72ac5329ed05a0df195e335e951820d20ca83433
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-06-18 15:45:12 +0200

    Updates

---
+++ precache-manifest.8512cc3055893a6bc2a29a2262c025f4.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ac6b69edd20ec591006149d4abc883",
+    "url": "/novea/index.html"
+  },
+  {
+    "revision": "3f32f7bb0487b2dbe7e6",
+    "url": "/novea/static/css/2.51585a46.chunk.css"
+  },
+  {
+    "revision": "3f32f7bb0487b2dbe7e6",
+    "url": "/novea/static/js/2.db54d6bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/novea/static/js/2.db54d6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b752601b0cdb1cc696",
+    "url": "/novea/static/js/main.5e47db32.chunk.js"
+  },
+  {
+    "revision": "e58587895b70632461ad",
+    "url": "/novea/static/js/runtime-main.888e20d7.js"
+  },
+  {
+    "revision": "732f0462dd2118422b37c829c864e0ff",
+    "url": "/novea/static/media/a.732f0462.svg"
+  },
+  {
+    "revision": "7053706f3404415715999bcbeee88b24",
+    "url": "/novea/static/media/aa.7053706f.svg"
+  },
+  {
+    "revision": "010677d0565a3670e0b827429fc24cd2",
+    "url": "/novea/static/media/aaa.010677d0.svg"
+  },
+  {
+    "revision": "29055ccdcf9c755f5e8ecb84e9c92702",
+    "url": "/novea/static/media/at-big.29055ccd.svg"
+  },
+  {
+    "revision": "916ea4e7a5de12e138314b84112037a8",
+    "url": "/novea/static/media/at.916ea4e7.svg"
+  },
+  {
+    "revision": "3f8ffcc7dcba2fc34191b6e2140e9ec2",
+    "url": "/novea/static/media/avatar.3f8ffcc7.png"
+  },
+  {
+    "revision": "a4654104c25a24cbf467334f4d82fa5f",
+    "url": "/novea/static/media/call-big.a4654104.svg"
+  },
+  {
+    "revision": "4fc18358476149faeb76b412d3c5a8e3",
+    "url": "/novea/static/media/call.4fc18358.svg"
+  },
+  {
+    "revision": "0a11601ff6c0590e7b473c97187bf259",
+    "url": "/novea/static/media/clinic.0a11601f.jpg"
+  },
+  {
+    "revision": "0e05430ed28c0985dba45b2bde3205a7",
+    "url": "/novea/static/media/doctor.0e05430e.svg"
+  },
+  {
+    "revision": "f9fb4fe6504e49f2d2b9861ffaf53651",
+    "url": "/novea/static/media/facebook.f9fb4fe6.svg"
+  },
+  {
+    "revision": "33203e8f8288394dd8fb49d041838a20",
+    "url": "/novea/static/media/flag.33203e8f.svg"
+  },
+  {
+    "revision": "50f4c059aba214b0a3c3c5569f65d1a7",
+    "url": "/novea/static/media/heartbeat.50f4c059.svg"
+  },
+  {
+    "revision": "059a0a4f028cfb0df785cde9f0a2d1e2",
+    "url": "/novea/static/media/logo-foot.059a0a4f.svg"
+  },
+  {
+    "revision": "c00109c2c70d70cc157843f9a928ae1b",
+    "url": "/novea/static/media/logo-nav.c00109c2.svg"
+  },
+  {
+    "revision": "07d756f49a53b448a3b00244c94a27b0",
+    "url": "/novea/static/media/pharmacy.07d756f4.svg"
+  },
+  {
+    "revision": "e27e76c5b420ae66de074ba4723777a9",
+    "url": "/novea/static/media/plus.e27e76c5.svg"
+  },
+  {
+    "revision": "20139afc5d30c4c72c5ed26790ed3d67",
+    "url": "/novea/static/media/quote.20139afc.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/novea/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/novea/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/novea/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "8641e4e9c5e7e6d19576e5fcfaf40337",
+    "url": "/novea/static/media/slide1.8641e4e9.jpg"
+  },
+  {
+    "revision": "4c8c21c9bc153ac0cc513dfa8eeb5bda",
+    "url": "/novea/static/media/slide2.4c8c21c9.jpg"
+  },
+  {
+    "revision": "d1e95349fd188f2ac871eac0a286c682",
+    "url": "/novea/static/media/slide3.d1e95349.jpg"
+  },
+  {
+    "revision": "7fac713572890edb50556ca344b81031",
+    "url": "/novea/static/media/spec1.7fac7135.png"
+  },
+  {
+    "revision": "b6fe39a97a18f31e1cb303957e57d7aa",
+    "url": "/novea/static/media/spec2.b6fe39a9.png"
+  },
+  {
+    "revision": "232cba31d58033b4b987169031407384",
+    "url": "/novea/static/media/spec3.232cba31.png"
+  }
+]);
